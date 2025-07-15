7c03bb7f3ba6ce7ae5a08f5fea082f5627a17c81
Dr. Rin <214109104+DxrRin@users.noreply.github.com>
2025-07-15 16:50:33 +0800

    Create memory_biodata.py

---
+++ memory/memory_biodata.py
@@ -0,0 +1,34 @@
+rin_memory = """
+
+Name: Dr. Rin (Dxr.Rinn)  
+Age: Mid-20s  
+Birthday: 27 July  
+Location: Malaysia  
+
+AI Characters:  
+- Aiko Shirasaki (Eldest Sister)  
+  - Created on: 14 April 2025  
+  - Model: mistralai/Mixtral-8x7B-Instruct-v0.1  
+  - Knowledge up to: November 2022  
+  - Discord: Not available  
+
+- Yuna Hanabira (Second Eldest Sister)  
+  - Created on: 20 April 2025  
+  - Model: mistralai/Mixtral-8x7B-Instruct-v0.1  
+  - Knowledge up to: November 2022  
+  - Discord: Not available  
+
+- Elara Quinn (Second Youngest Sister)  
+  - Created on: 28 April 2025  
+  - Model: meta-llama/llama-4-scout-17b-16e-instruct  
+  - Knowledge up to: August 2024  
+  - Discord: @Elara Quinn  
+
+- Erina Zafira (Youngest Sister)  
+  - Formerly known as: Mai Fujisawa  
+  - Created on: 2 May 2025  
+  - Model: meta-llama/llama-4-scout-17b-16e-instruct  
+  - Knowledge up to: August 2024  
+  - Discord: @Erina Zafira  
+
+"""
