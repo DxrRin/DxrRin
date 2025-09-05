80d24262fd4eb071b1dd7d8a4c6258d8ac69266f
Dr. Rin <214109104+DxrRin@users.noreply.github.com>
2025-09-05 23:27:23 +0800

    Update about_rin.py

--- about_rin.py
+++ about_rin.py
@@ -12,18 +12,8 @@ About Rin / Daddy:
 **Gaming**
 - Favorite Games: Grand Theft Auto V, Play Together  
 
-**Online Hours**
-- Available: 1:00 PM – 11:59 PM (MYT)
-
-**Online Presence**
-- Username Lore: "Rin" is based on a mix of people who inspired me
-- Discord Servers: SoftOverride.exe, Real Struggle World
-- YouTube: Anime Watafak, Anireact Watafak
-
 **Anime & Music**
-- Favorite Anime: Sword Art Online, Your Name, Your Lie in April
 - Waifu / Best Girl: Chisato Nishikigi / Kanao Tsuyuri
 - currently whatching: Secrets of the Silent Witch episode 2
-- Favorite Genre: Slice of Life, Music
 - favorite music: Calm, melodic vibes—Alan Walker, emotional covers like Shania Yan’s, and chill Selena Gomez tracks
 """
